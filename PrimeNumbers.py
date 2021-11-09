2b4853e00866642950aaef718b10a74700392ba7
sarawaniolka <92310096+sarawaniolka@users.noreply.github.com>
2021-11-09 13:48:47 +0100

    Update PrimeNumbers.py

--- PrimeNumbers.py
+++ PrimeNumbers.py
@@ -1,9 +1,9 @@
 primeNums = []
-x = 27
+N = 27
 
-for i in range (2, x+1):
+for i in range (2, N+1):
     c=0
-    for j in range (2,x+1):
+    for j in range (2,N+1):
         if i%j == 0:
             c+=1
     if c==1:
