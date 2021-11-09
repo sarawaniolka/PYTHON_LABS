dae8cfe4024557c4a0602959b9f464bdbbdc126d
sarawaniolka <waniolka.sara@gmail.com>
2021-11-09 13:45:32 +0100

    Prime Numbers

---
+++ PrimeNumbers.py
@@ -0,0 +1,11 @@
+primeNums = []
+x = 27
+
+for i in range (2 ,x+1):
+    c=0
+    for j in range (2,x+1):
+        if i%j == 0:
+            c+=1
+    if c==1:
+        primeNums.append(i)
+print(primeNums)
