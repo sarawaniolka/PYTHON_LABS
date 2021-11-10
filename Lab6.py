aeab98d6b43e9dc0e1a03f7f9fc96cd54007f24c
sarawaniolka <waniolka.sara@gmail.com>
2021-11-10 16:00:48 +0100

    lab6

--- Lab6.py
+++ Lab6.py
@@ -1,9 +1,15 @@
+# global vs local variables
 def sum(a,b):
     print('a=' + str(a))
     print('b=' + str(b))
+    a = 100
     return a+b
 
 a = 5
 b = 10
 c = sum(a,b)
 print(c)
+
+# =======================================
+# lists, tuples, dicts, arrays
+
