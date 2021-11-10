da35c22258fb1a7d258966e24e31842067152e5b
sarawaniolka <waniolka.sara@gmail.com>
2021-11-10 16:07:51 +0100

    lab6

--- Lab6.py
+++ Lab6.py
@@ -1,4 +1,6 @@
-# global vs local variables
+print("=======================================")
+print("global vs local variables")
+print("=======================================")
 def sum(a,b):
     print('a=' + str(a))
     print('b=' + str(b))
@@ -10,6 +12,23 @@ b = 10
 c = sum(a,b)
 print(c)
 
-# =======================================
-# lists, tuples, dicts, arrays
+print("=======================================")
+print("lists, tuples, dicts, arrays")
+print("=======================================")
+
+
+l1 = []
+l2 = [2323, 23324, 24454365,67657, 75775]
+print(l2[3])
+print(l2[-3:-1])
+
+i=0
+for elem in l2:
+    print('Element {} at {}'.format(elem,i))
+    i += 1
+
+print(l2.index(2323))
+
+l2.insert(0,1)
+print(l2)
 
