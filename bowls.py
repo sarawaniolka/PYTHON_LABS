67fa7608f2de403720456fc1803ae4d3b48f2791
sarawaniolka <waniolka.sara@gmail.com>
2021-11-26 23:46:29 +0100

    recursion

---
+++ bowls.py
@@ -0,0 +1,12 @@
+n = 5
+sum = (n *(n+1)) / 2
+print(sum)
+
+#recursion
+def sum_bowls_recursion(r):
+    if r == 1:
+        return 1
+    else:
+        return r + sum_bowls_recursion(r-1)
+
+print(sum_bowls_recursion(5))
