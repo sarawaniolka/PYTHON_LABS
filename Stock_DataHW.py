324f33464a37168f82098aa5ed7cb43b696833cc
sarawaniolka <waniolka.sara@gmail.com>
2021-12-10 19:55:52 +0100

    HW1

--- Stock_DataHW.py
+++ Stock_DataHW.py
@@ -1,61 +1,47 @@
 import urllib.request
 import os
-from os import listdir
 
 dir_name = os.getcwd()
 
-# 1. Downloading the files
-url = 'https://query1.finance.yahoo.com/v7/finance/download/GOOG?period1=1607611052&period2=1639147052&interval=1d' \
-      '&events=history&includeAdjustedClose=true '
-with urllib.request.urlopen(url) as response, open("GOOG.csv", 'wb') as out_file:
-    data = response.read()  # a `bytes` object
-    out_file.write(data)
+#Downloading the file
+def download_file(name):
+    url = 'https://query1.finance.yahoo.com/v7/finance/download/'+name+'?period1=1587042293&period2=1618578293&interval=1d&events=history&includeAdjustedClose=true'
+    with urllib.request.urlopen(url) as response, open(name + '.csv', 'wb') as out_file:
+        out_file.write(response.read())
 
-url = 'https://query1.finance.yahoo.com/v7/finance/download/IBM?period1=1607611129&period2=1639147129&interval=1d' \
-      '&events=history&includeAdjustedClose=true '
-with urllib.request.urlopen(url) as response, open("IBM.csv", 'wb') as out_file:
-    data = response.read()  # a `bytes` object
-    out_file.write(data)
-
-url = 'https://query1.finance.yahoo.com/v7/finance/download/MSFT?period1=1607611176&period2=1639147176&interval=1d' \
-      '&events=history&includeAdjustedClose=true '
-with urllib.request.urlopen(url) as response, open("MSFT.csv", 'wb') as out_file:
-    data = response.read()  # a `bytes` object
-    out_file.write(data)
-
-
-# 2. Searching for .csv files
+#Finding csv files in directory
 def find_csv(path_to_dir, suffix=".csv"):
-    filenames = listdir(path_to_dir)
+    filenames = os.listdir(path_to_dir)
     return [filename for filename in filenames if filename.endswith(suffix)]
 
+#Calculating the change value and adding to data
+def calculate_change(file):
+    out_data = []
+    with open(file, 'r') as f:
+        rows = f.readlines()
+        headers = rows.pop(0)
+        out_data.append(headers.strip().split(',') + ['Change'])
+    for row in rows:
+        indata = row.strip().split(',')
+        opening = float(indata[1])
+        closing = float(indata[4])
+        change = (closing - opening)/closing
+        out_data.append(indata + [str(change)])
+    return out_data
 
-print(find_csv(dir_name))
-
-# 3. Calculating the percentage change between Close and Open price and
-# adding these values as another column to this CSV file.
-
-for file in find_csv(dir_name):
-    csv_file = open(file)
-    Closings = [] #list for closing values
-    Openings = [] #list for opening values
-    for row in csv_file:
-        Closings.append(row.split(',')[4]) #add a closing value to the list
-        Openings.append(row.split(',')[1]) #add an opening value to the list
-    Closings.pop(0) #delete the header from closing values
-    Openings.pop(0) #delete the header from opening values
-    Closings = [*map(float, Closings)] #change closing values to a float
-    Openings = [*map(float, Openings)] #change opening values to a float
-    for x in range(0, len(Closings)): #for every row in a file
-        Change = str((Closings[x]-Openings[x])/Closings[x]) #calculate the change
-        print(Change)
-
-
-
-
+#Writing the file
+def save_file(file,data):
+    if os.path.exists(file):
+        os.remove(file)
+    with open(file, 'a') as f:
+        for x in data:
+            f.write(','.join(x) + '\n')
 
 
-
-
-
-
+#Calling functions
+if __name__ == '__main__':
+    files = ['GOOG','IBM', 'MSFT']
+    for z in files:
+        download_file(z)
+    for file in find_csv(dir_name):
+        save_file(file, calculate_change(file))
