9e216bb6c24d0e04a8a87ace260fe456b15abfac
sarawaniolka <waniolka.sara@gmail.com>
2021-12-10 15:52:14 +0100

    HW1

--- Stock_DataHW.py
+++ Stock_DataHW.py
@@ -1,5 +1,8 @@
 import urllib.request
+import os
+from os import listdir
 
+dir_name = os.getcwd()
 
 # 1. Downloading the files
 url = 'https://query1.finance.yahoo.com/v7/finance/download/GOOG?period1=1607611052&period2=1639147052&interval=1d' \
@@ -20,4 +23,11 @@ with urllib.request.urlopen(url) as response, open("MSFT.csv", 'wb') as out_file
     data = response.read()  # a `bytes` object
     out_file.write(data)
 
+
 # 2. Searching for .csv files
+def find_csv(path_to_dir, suffix=".csv"):
+    filenames = listdir(path_to_dir)
+    return [filename for filename in filenames if filename.endswith(suffix)]
+
+
+print(find_csv(dir_name))
