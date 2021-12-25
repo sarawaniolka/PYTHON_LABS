7ad33c05272c9905d498e25385041674c299a989
sarawaniolka <92310096+sarawaniolka@users.noreply.github.com>
2021-12-25 15:18:11 +0100

    matrices

--- Matrices HW.py
+++ Matrices HW.py
@@ -14,4 +14,18 @@ print(arr)
 #Create a 10×10 matrix of zeros and then "frame" it with a border of ones.
 import numpy as np
 x =  np.zeros((10,10),dtype = int)
+rows = len(x)
+cols = len(x[0])
+
+for r in range(rows):
+    for c in range(cols):
+        if r ==0:
+            x[r,c]=1
+        elif r==9:
+            x[r,c]=1
+        elif c==0:
+            x[r,c]=1
+        elif c==9:
+            x[r,c]=1
+
 print(x)
