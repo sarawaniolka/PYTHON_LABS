7948a81c02d18a05c3ef61ba4db9bc3070029557
sarawaniolka <92310096+sarawaniolka@users.noreply.github.com>
2021-12-25 14:55:15 +0100

    matrices

---
+++ Matrices HW.py
@@ -0,0 +1,17 @@
+#Compute the multiplication of the following matrices:
+
+M1 = [[1, 0], [0, 1]]
+M2 = [[1, 2], [3, 4]]
+print(M1)
+print(M2)
+
+#Generate an array of length 3n filled with the cyclic pattern 1, 2, 3.
+
+n = 2
+arr = [1,2,3] * n
+print(arr)
+
+#Create a 10×10 matrix of zeros and then "frame" it with a border of ones.
+import numpy as np
+x =  np.zeros((10,10),dtype = int)
+print(x)
